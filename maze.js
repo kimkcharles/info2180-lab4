dc35353b16dae619b12f77abda512876eef06dac
Kimberly <kimmy_khamille@yahoo.com>
2018-10-20 04:47:36 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -1,9 +1,26 @@
 window.onload = function() {
-
+	//Excercise 1
 	let wall= document.getElementById("boundary1");
 	wall.addEventListener("mouseover", function() {
-	  this.classList.add("youlose");
+	 	this.classList.add("youlose");
 	});
+
+		
+	//Excercise 2
+	
+	let walls= document.querySelectorAll(".boundary:not(.example)");
+
+	
+	for (let w=0; w<walls.length; w++){
+			walls[w].addEventListener("mouseover", function(){
+				for (a=0; a<walls.length ; a++){
+					walls[a].classList.add("youlose");
+				}
+			})
+		}
+
+
+	
 
 
 }
