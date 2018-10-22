4e399a06d79918b0344caacfbcf4cc29682faeac
Kimberly <kimmy_khamille@yahoo.com>
2018-10-22 11:23:33 -0500

    Exercise 6

--- maze.js
+++ maze.js
@@ -2,15 +2,25 @@ window.onload = function() {
 
 	//This is for if the boundary is touched
 	var touched=false;
+	//This is for if the status message has been presented
+	var stat=false;
 	var statmess=document.getElementById("status");
-
+	var startMaze=false;
+	//var touchedwalls= document.querySelectorAll(".youlose")
 
 
 	//Excercise 1
+	/*
 	let wall= document.getElementById("boundary1");
 	wall.addEventListener("mouseover", function() {
-	 	this.classList.add("youlose");
+		if (startMaze==true && stat==false){
+	 		this.classList.add("youlose");
+	 		stat=true;
+			statmess.innerHTML="You Lose!"; //Exercise 5
+			touched=true;
+	 	}
 	});
+	*/
 
 		
 	//Excercise 2
@@ -20,11 +30,15 @@ window.onload = function() {
 	
 	for (let w=0; w<walls.length; w++){
 		walls[w].addEventListener("mouseover", function(){
-			statmess.innerHTML="You Lose!"; //Exercise 5
-			for (a=0; a<walls.length ; a++){
-				walls[a].classList.add("youlose");
+			if (startMaze==true && stat==false){
+				stat=true;
+				statmess.innerHTML="You Lose!"; //Exercise 5
+				touched=true;
+
+				for (a=0; a<walls.length ; a++){
+					walls[a].classList.add("youlose");
+				}
 			}
-			touched=true;
 		})
 	}
 
@@ -32,12 +46,12 @@ window.onload = function() {
 	//Excercise 3
 	let ended = document.getElementById("end");
 	ended.addEventListener("mouseover", function(){
-		if (touched==false){
-			//alert("You Win!");
-			statmess.innerHTML="You Win!"; //Exercise 5
+		if (touched==false && stat==false && startMaze==true){
+				statmess.innerHTML="You Win!"; //Exercise 5
+				stat=true;
+				//alert("You Win!");
 		}
 	});
-
 
 
 	//Excercise 4
@@ -45,17 +59,44 @@ window.onload = function() {
 	let begin =document.getElementById("start");
 	begin.addEventListener("click", function(){
 		//alert("Lets start!!");
-
+		stat=false;
+		startMaze=true;
 		touched=false;
-		if (walls.length!=0){
+
+		if (document.querySelectorAll(".youlose").length!=0){
 			walls.forEach(function(e){
 				e.classList.remove("youlose");
 			})
 		}
 	});
 
+		
+	//Excercise 6
+	
+	var original= 'Move your mouse over the "S" to begin.';
+	begin.addEventListener("mouseover", function(){
+		if(document.querySelectorAll(".youlose").length==0 && statmess.innerHTML==original){
+			startMaze=true;
+			stat=false;
+			touched=false;			
+		}
+	});
 
-
+	var maze= document.getElementById("maze");
+	begin.addEventListener("mouseout", function(){
+		
+		maze.addEventListener("mouseleave", function(){
+			
+			if (startMaze==true && stat==false && touched==false){
+				statmess.innerHTML="You Lose!";
+				for (a=0; a<walls.length ; a++){
+					walls[a].classList.add("youlose");
+				}
+				stat=true;
+				touched=true;
+			}
+		});
+	});
 
 
 	
